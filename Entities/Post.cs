98d1e7d9fb93af92fd85ff0022fecde1e6862c10
Victor Meldgaard <49498101+meldg0d@users.noreply.github.com>
2024-09-09 17:46:02 +0200

    part 1

---
+++ Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace GithubTest;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int UserId { get; set; }
+
+    public Post(int id, string title, string body, int userId)
+    {
+        Id = id;
+        Title = title;
+        Body = body;
+        UserId = userId;
+    }
+}
--- GithubTest/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GithubTest;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
