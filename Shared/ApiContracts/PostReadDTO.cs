46f08ced718dafbf499dffd955f3cd14e119e5d4
Victor Meldgaard <49498101+meldg0d@users.noreply.github.com>
2024-11-29 09:14:25 +0100

    Delete Shared directory

--- Shared/ApiContracts/PostReadDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiContracts;
-
-public class PostReadDTO
-{
-    public int Id { get; set; }   
-    public string Title { get; set; }
-    public string Body { get; set; }
-    public int UserId { get; set; }
-}
--- Shared/ApiContracts/UserReadDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiContracts;
-
-public class UserReadDTO
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
